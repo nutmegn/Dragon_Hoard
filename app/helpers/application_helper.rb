7fb495fada1360ed07f56c3812eec516dd2667d3
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-18 17:53:40 +0000

    Added collection based partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,8 +11,8 @@ module ApplicationHelper
 
   def source_helper(layout_name)
     if session[:source]
-    greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name layout"
-    content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
     end
   end
 end
