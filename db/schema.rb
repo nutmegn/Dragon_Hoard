ad4a7540dc625385e4c196e707f251bbf2fc4f8d
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-19 14:33:43 +0000

    Fixed technologies error

--- db/schema.rb
+++ db/schema.rb
@@ -54,9 +54,9 @@ ActiveRecord::Schema.define(version: 20170705143355) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "technologies", id: :serial, force: :cascade do |t|
+  create_table "technologies", force: :cascade do |t|
     t.string "name"
-    t.integer "portfolio_id"
+    t.bigint "portfolio_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
