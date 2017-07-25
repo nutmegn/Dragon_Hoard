0d3c8a89c6bfe91b8476edb9740e14123882420b
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-25 23:24:03 +0000

    fixed rails versions

--- db/migrate/20170627165923_add_topic_reference_to_blogs.rb
+++ db/migrate/20170627165923_add_topic_reference_to_blogs.rb
@@ -1,4 +1,4 @@
-class AddTopicReferenceToBlogs < ActiveRecord::Migration[5.1]
+class AddTopicReferenceToBlogs < ActiveRecord::Migration[5.0]
   def change
     add_reference :blogs, :topic #foreign_key: true
   end
