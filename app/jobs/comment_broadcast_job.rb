1a61dbeb2e8f894c05472f1616c0ba765a86c92f
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-08-07 15:45:58 +0000

    Implimented action cable for comments

--- app/jobs/comment_broadcast_job.rb
+++ app/jobs/comment_broadcast_job.rb
@@ -1,13 +1,13 @@
 class CommentBroadcastJob < ApplicationJob
   queue_as :default
-  
+
   def perform(comment)
     ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment(comment)
   end
-  
-  private 
-  
+
+  private
+
   def render_comment(comment)
-    CommentsController.render partial: 'comment/comments', locals: { comment: comment }
+    CommentsController.render partial: 'comments/comment', locals: { comment: comment }
   end
 end
