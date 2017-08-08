2527e4ffc3630675699877f86b2e2e00f95f104b
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-08-08 00:57:16 +0000

    ...

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,14 @@ class Topic < ApplicationRecord
     validates_presence_of :title
     
     has_many :blogs
+    
+    def self.with_blogs
+        includes(:blogs).where.not(blogs: { id: nil })
+    end
+end
+
+blog_filled_arry = []
+topics = Topic.all
+topics.each do |topic|
+    blog_filled_arry << topic if topic.blogs.count > 0
 end
